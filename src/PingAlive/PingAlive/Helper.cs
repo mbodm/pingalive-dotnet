b629acca644233a248c3a0e0e8a6e146d69ba1e9
MBODM <webmaster@mbodm.com>
2023-08-10 00:09:44 +0200

    Initial commit.

---
+++ src/PingAlive/PingAlive/Helper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace PingAlive
+{
+    internal class Helper
+    {
+        public static string GetAppName()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Name;
+        }
+
+        public static string GetAppVersion()
+        {
+            // This project is using the newer "SDK style project format", which became a default since .NET Core:
+            // - Inside of .csproj file the <Version> tag is used for the so-called "Package Version", by default.
+            // - In VS2022 see menu -> Project -> [PROJ-NAME] Properties -> Package -> General -> Package Version.
+            // - The "AssemblyInformationalVersionAttribute" can be used to access that project-setting from code.
+
+            // For more information please take a look at either the post of Edi Wang, or the post of Andrew Lock:
+            // - https://edi.wang/post/2018/9/27/get-app-version-net-core
+            // - https://andrewlock.net/version-vs-versionsuffix-vs-packageversion-what-do-they-all-mean/
+
+            return Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+
+        public static void ShowSuccess()
+        {
+            Console.WriteLine("Success:");
+            Console.WriteLine("- Successfully started ping command (ping -t 194.25.2.129) in a separate window");
+            Console.WriteLine("- The ping command is running until you press CTRL+C there or close that window");
+            Console.WriteLine();
+            Console.WriteLine("Have a nice day.");
+        }
+
+        public static void ShowError(params string[] errorMessages)
+        {
+            Console.WriteLine("Error:");
+            errorMessages.ToList().ForEach(errorMessage => Console.WriteLine($" - {errorMessage}"));
+            Console.WriteLine();
+            Console.WriteLine("Please try again, or create a GitHub issue if you can´t solve the problem.");
+        }
+    }
+}
---
+++ src/PingAlive/PingAlive/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PingAlive
+{
+    internal class Program
+    {
+        private const string ReleaseDate = "08/2023";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{Helper.GetAppName()}.exe {Helper.GetAppVersion()} (by MBODM {ReleaseDate})");
+            Console.WriteLine();
+            Console.WriteLine("A tiny Windows command-line tool to continuously ping Telekom´s primary DNS server");
+            Console.WriteLine();
+            Console.WriteLine("For more information just take a look at https://github.com/mbodm/pingalive-dotnet");
+            Console.WriteLine();
+
+            if (args.Any())
+            {
+                Helper.ShowError("Arguments are not supported", "Please restart executable without any command-line arguments");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                // No validation here if cmd, start and ping really exist.
+                // If not, you have a more serious problem than this, imo.
+                // But i tested those scenarios and error-behavior was ok.
+
+                var process = Process.Start("ping", "-t 194.25.2.129");
+            }
+            catch (Exception e)
+            {
+                Helper.ShowError("Starting ping command failed", $"Exception-Message was \"{e.Message}\"");
+                Environment.Exit(2);
+            }
+
+            Helper.ShowSuccess();
+            Environment.Exit(0);
+        }
+    }
+}
